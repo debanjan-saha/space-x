5f534a2990f8ba1cd5dae3ac6f5809968b8187ba
Debanjan Saha <debanjan.saha@nice.com>
2020-11-19 00:41:29 +0530

    added fixes

--- src/app/spacex-api.service.ts
+++ src/app/spacex-api.service.ts
@@ -1,16 +1,16 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { throwError } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
 import { Launch } from './launch.model';
 
 const BASE_URL = 'https://api.spaceXdata.com/v3/launches?limit=100';
 
-@Injectable({ providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class SpaceXService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
-  getLaunches(year: number, launchSuccess: boolean, landSuccess: boolean) {
+  getLaunches(year: number, launchSuccess: boolean, landSuccess: boolean): Observable<Launch[]> {
     const url = this.prepareUrl(year, launchSuccess, landSuccess);
     return this.http.get<Launch[]>(url).pipe(catchError(error => {
       console.error('Failed to get list of SpaceX launches!!', error);
@@ -18,7 +18,7 @@ export class SpaceXService {
     }));
   }
 
-  private prepareUrl(year: number, launchSuccess: boolean, landSuccess: boolean) {
+  private prepareUrl(year: number, launchSuccess: boolean, landSuccess: boolean): string {
     let url = BASE_URL;
     if (year) {
       url = `${url}&launch_year=${year}`;
