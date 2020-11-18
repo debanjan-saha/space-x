9cb540ee406b553cb4d23d913c472cd4ce2b7ae3
Debanjan Saha <debanjan.saha@nice.com>
2020-11-18 20:22:04 +0530

    fix

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/space-x'));
+app.use(express.static(__dirname + '/dist/space-x/browser'));
 
 app.get('/*', function(req, res) {
 
-res.sendFile(path.join(__dirname+'/dist/space-x/index.html'));
+res.sendFile(path.join(__dirname+'/dist/space-x/browser/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
